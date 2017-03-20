1952fc2356da07d30452a7e7ceb826c8754e4958
soeru <soeda.yuki@gmail.com>
2017-03-20 15:34:19 +0900

    Initial Commit

---
+++ Warikan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Warikan
+//
+//  Created by 添田祐輝 on 2017/03/20.
+//  Copyright © 2017年 添田祐輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
